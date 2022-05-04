2a0188ac5ec14e179668836b1237884ddfe653b5
Mervin John C. Tampus <79685609+jdesolate@users.noreply.github.com>
2022-05-04 08:15:20 +0800

    v1.1 Navbar and Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,10 +7,14 @@ function Header() {
 			<div className='header'>
 				<div className='header-container'>
 					<div className='header-container-col1'>
-						<h1>Hey I'm</h1>
+						<h1 className='header-container-col1-text1'>Hey I'm</h1>
 						<h1 className='header-container-col1-name'>Mervin John Tampus</h1>
-						<h3>Aspiring Software Engineer</h3>
-						<h4>UI/UX, Flutter, React JS, Firebase</h4>
+						<h2 className='header-container-col1-text2'>
+							Aspiring Software Engineer
+						</h2>
+						<h2 className='header-container-col1-text3'>
+							UI/UX, Flutter, React JS, Firebase
+						</h2>
 						<button>My Works</button>
 					</div>
 					<div className='header-container-col2'>
