8de354d6ca06aadfe422023524b8e3729cceead8
Mervin John C. Tampus <79685609+jdesolate@users.noreply.github.com>
2022-05-09 16:17:09 +0800

    v1.3 Tech Stack

---
+++ src/components/techstack/Techstack.tsx
@@ -0,0 +1,32 @@
+import '../../main.scss';
+
+function Techstack() {
+	return (
+		<>
+			<div className='techStack'>
+				<div className='techStack-cont1'>
+					<h1>Tech Stack</h1>
+					<p>
+						During the past 2 years, I've worked with a variety of technologies.
+					</p>
+				</div>
+				<div className='techStack-cont2'>
+					<img src='../../../../assets/images/Flutter.png' alt='Flutter logo' />
+					<img
+						src='../../../../assets/images/Firebase.png'
+						alt='Firebase logo'
+					/>
+					<img src='../../../../assets/images/React.png' alt='React logo' />
+					<img src='../../../../assets/images/Javascript.png' alt='JS logo' />
+					<img src='../../../../assets/images/Typescript.png' alt='TS logo' />
+					<img src='../../../../assets/images/Sass.png' alt='Sass logo' />
+					<img src='../../../../assets/images/Clogo.png' alt='C logo' />
+					<img src='../../../../assets/images/Csharplogo.png' alt='C# logo' />
+					<img src='../../../../assets/images/Python.png' alt='Python logo' />
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Techstack;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './components/header';
-import Navbar from './components/navbar';
+import Header from './components/header/header';
+import Navbar from './components/navbar/navbar';
+import Techstack from './components/techstack/Techstack';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Navbar />
 		<Header />
+		<Techstack />
 	</React.StrictMode>
 );
